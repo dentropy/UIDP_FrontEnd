c70234d7037d597ad38c87ab0e98b2e1b5bddf62
Windows X230 <paulmullins@protonmail.com>
2018-06-23 18:22:00 -0400

    Added migrations to the truffle configuration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Identity = artifacts.require("./Identity.sol");
+var multisig = artifacts.require("./multisig.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Identity);
+  deployer.deploy(multisig);
 };

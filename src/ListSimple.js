3e263ffcca96b9c5168f73f2c4ccf344fe94c790
Ralston Jovel Almeida <ralstona@ca.ibm.com>
2018-06-24 10:16:04 -0400

    Add routing to app

--- src/ListSimple.js
+++ src/ListSimple.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import MobileTearSheet from './MobileTearSheet';
+import {withRouter} from 'react-router-dom';
 import {List, ListItem} from 'material-ui/List';
 import ContentInbox from 'material-ui/svg-icons/content/inbox';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 import ContentSend from 'material-ui/svg-icons/content/send';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import Divider from 'material-ui/Divider';
-import ActionInfo from 'material-ui/svg-icons/action/info';
 
-const ListSimple = () => (  
+const ListSimple = withRouter(({ history }) => (
   <div >
     <List style={{ width: "225px"}}>
-      <ListItem primaryText="Details" leftIcon={<ContentInbox />} />
-      <ListItem primaryText="Credentials" leftIcon={<ActionGrade />} />
-      <ListItem primaryText="Marketplace" leftIcon={<ContentSend />} />
+      <ListItem onClick={() => { history.push('/') }} primaryText="Details" leftIcon={<ContentInbox />} />
+      <ListItem onClick={() => { history.push('/credentials') }} primaryText="Credentials" leftIcon={<ActionGrade />} />
+      <ListItem onClick={() => { history.push('/marketplace')}} primaryText="Marketplace" leftIcon={<ContentSend />} />
     </List>
   </div>
-);
+));
 
 export default ListSimple;
